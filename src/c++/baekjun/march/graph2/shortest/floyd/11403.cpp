a4f2332191a95a131986ae6701ae219d30bb89dc
BbolroC <bbolroc@gmail.com>
2016-03-25 05:39:07 +0900

    Floyd in progress

---
+++ src/c++/baekjun/march/graph2/shortest/floyd/11403.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+using namespace std;
+
+int a[101][101];
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (a[i][k] == 1 && a[k][j] == 1) {
+					a[i][j] = 1;
+				}
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
